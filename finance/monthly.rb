0d3a71d45b72eb2ce1baae930f4c83fd987f1275
jordanmking <jordanm_king@yahoo.com>
2017-08-19 04:05:36 -0500

    0_o

--- finance/monthly.rb
+++ finance/monthly.rb
@@ -1,8 +1,8 @@
-bills = {'carecredit'=>27,'creditcard'=>40,'meds/doc'=>45,'chowder'=>20,'insurance'=>0,'phone'=>0,'stor'=>90,'amazon'=>12,'food'=>180,'gas'=>40,
+bills = {'carecredit'=>27,'creditcard'=>0,'meds/doc'=>45,'chowder'=>20,'insurance'=>0,'phone'=>0,'stor'=>90,'amazon'=>12,'food'=>100,'gas'=>15,
 'car insurance'=>60,'banfield'=>80,'dog food and flea'=>30,'photoshop'=>22}
 bills2 = {'rent'=>1000,'electric'=>100,'internet'=>80}
 bills3 = {'emergencyfund'=>250,'unexpected fund'=>250,'computerfund'=>150,'courtney'=>150,'jet'=>50,'entertainment'=>100,'vacation'=>150,'xmas/bday'=>200}
-unexpected = {'internetsetup'=>45,'schoolsupp'=>80,'movingtruck'=>0,'prorated'=>675,'unexpected'=>0}
+unexpected = {'internetsetup'=>45,'schoolsupp'=>80,'movingtruck'=>0,'prorated'=>0,'unexpected'=>0,'rentersinsurance'=>0}
 debt = 0
 bills.each_value{|x| debt += x}
 bills2.each_value{|x| debt += x}
@@ -10,7 +10,7 @@ bills2.each_value{|x| debt += x}
 unexpected.each_value{|x|debt+=x}
 ##puts 'your debt each month is'
 ##puts debt
-pay = 3000
+pay = 1930
 ##puts 'your left over is'
 left = pay - debt 
 puts left
